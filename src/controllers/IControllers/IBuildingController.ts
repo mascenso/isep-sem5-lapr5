83f77aa528c67de154ccd4880a5dbb0d7d38709f
MiguelCCardoso <Miguel.ccardoso92@gmail.com>
2023-10-22 01:16:23 +0100

    [US-160] Adicionado um mecanismo de edição do edificio através do comando PATCH (LPR5-2)

--- src/controllers/IControllers/IBuildingController.ts
+++ src/controllers/IControllers/IBuildingController.ts
@@ -2,4 +2,5 @@ import {NextFunction, Request, Response} from "express";
 
 export default interface IBuildingController {
   createBuilding(req: Request, res: Response, next: NextFunction);
+  updateBuilding(req: Request, res: Response, next: NextFunction);
 }
--- src/services/buildingService.ts
+++ src/services/buildingService.ts
@@ -6,6 +6,7 @@ import {IBuildingDTO} from "../dto/IBuildingDTO";
 import {Result} from "../core/logic/Result";
 import {BuildingMap} from "../mappers/BuildingMap";
 import {Building} from "../domain/building";
+import { ConnectionCheckedOutEvent } from 'mongodb';
 
 
 @Service()
@@ -29,6 +30,42 @@ export default class BuildingServiceService implements IBuildingService {
 
       const buildingDTOResult = BuildingMap.toDTO( buildingResult ) as IBuildingDTO;
       return Result.ok<IBuildingDTO>( buildingDTOResult )
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  public async updateBuilding(buildingDTO: IBuildingDTO): Promise<Result<IBuildingDTO>> {
+    try {
+
+      let building = await this.buildingRepo.findByDomainId(buildingDTO.id);
+
+      if (building === null) {
+        return Result.fail<IBuildingDTO>('Building not found');
+      }
+
+      if (buildingDTO.name) {
+        building.name = buildingDTO.name;
+      }
+      if (buildingDTO.description) {
+        building.description = buildingDTO.description;
+      }
+      if (buildingDTO.maxLength) {
+        building.maxLength = buildingDTO.maxLength;
+      }
+      if (buildingDTO.maxWidth) {
+        building.maxWidth = buildingDTO.maxWidth;
+      }
+      if (buildingDTO.code) {
+        building.code = buildingDTO.code;
+      }
+
+      await this.buildingRepo.save(building);
+  
+      const buildingDTOResult = BuildingMap.toDTO(building) as IBuildingDTO;
+      return Result.ok<IBuildingDTO>(buildingDTOResult);
+
+
     } catch (e) {
       throw e;
     }

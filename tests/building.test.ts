ec1bbaa09483cf11842b230b1775309e7da498cc
MiguelCCardoso <Miguel.ccardoso92@gmail.com>
2023-10-25 22:19:13 +0100

    [US-160] Adicionado testes para Building e floor (LPR5-2)

--- tests/building.test.ts
+++ tests/building.test.ts
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import {Building} from '../src/domain/building'  // Adjust the path to the Building class
+import {Building} from '../src/domain/building'  
 
 describe('Building', () => {
   it('should create a valid Building instance', () => {
@@ -34,4 +34,36 @@ describe('Building', () => {
     expect(buildingResult.isFailure).to.be.true;
     expect(buildingResult.error).to.not.be.undefined;
   });
+
+  it('should be possible edit the building props', () => {
+    const buildingProps = {
+      code: 'B001',
+      maxWidth: 10,
+      maxLength: 20,
+      name: 'Sample Building',
+      description: 'A test building',
+    };
+
+    const buildingResult = Building.create(buildingProps);
+    const building = buildingResult.getValue();
+
+    expect(building.maxWidth).to.equal(10);
+    expect(building.maxLength).to.equal(20);
+    expect(building.code).to.equal("B001");
+    expect(building.description).to.equal('A test building');
+    expect(building.name).to.equal('Sample Building');
+
+    building.maxLength = 30;
+    building.maxWidth = 30;
+    building.code = "B002";
+    building.name = "New building";
+    building.description = "New description";
+
+    expect(building.maxWidth).to.equal(30);
+    expect(building.maxLength).to.equal(30);
+    expect(building.code).to.equal("B002");
+    expect(building.description).to.equal('New description');
+    expect(building.name).to.equal('New building');
+  });
 });
+

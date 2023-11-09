80a0cee14b6764879fb9b747f3461bf024c74c6f
Luís Matos <1211804@isep.ipp.pt>
2023-11-09 21:41:06 +0000

    Adicionado componente principal com sidenav e toolbar.

---
+++ ui/src/app/home/home.component.ts
@@ -0,0 +1,19 @@
+import { Component, inject } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  private breakpointObserver = inject(BreakpointObserver);
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+}

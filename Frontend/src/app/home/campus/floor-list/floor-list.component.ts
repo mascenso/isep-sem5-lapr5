69cfe0465c4722b351d0dcf9cd68afe0a28e3c4a
Luís Matos <1211804@isep.ipp.pt>
2023-11-12 20:43:25 +0000

    [US1160] Adicionada tabela com dados de floor mockados. (LPR5-52).

--- Frontend/src/app/home/campus/floor-list/floor-list.component.ts
+++ Frontend/src/app/home/campus/floor-list/floor-list.component.ts
@@ -1,10 +1,18 @@
 import {Component, OnInit} from '@angular/core';
 import {FormControl} from "@angular/forms";
-import {BuildingResponseDto, BuildingService} from "../../../services/building.service";
+import { BuildingService } from "../../../services/building.service";
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-floor-list',
   templateUrl: './floor-list.component.html',
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed,void', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
   styleUrls: ['./floor-list.component.css']
 })
 export class FloorListComponent implements OnInit {
@@ -28,6 +36,11 @@ export class FloorListComponent implements OnInit {
     }
   ];
 
+  dataSource = FLOOR_DATA;
+  columnsToDisplay = ['id', 'floorNumber', 'width', 'length'];
+  columnsToDisplayWithExpand = [...this.columnsToDisplay, 'expand'];
+  expandedElement: FloorDTO | null | undefined;
+
   buildingSelectionControl =  new FormControl();
 
   constructor(private buildingService: BuildingService) {}
@@ -36,4 +49,51 @@ export class FloorListComponent implements OnInit {
     // fetch building list from service
   }
 
+  onSelectionUpdateTable(selection: any) {
+
+  }
+
 }
+
+export interface FloorDTO {
+  id: string;
+  buildingId:string;
+  width: number;
+  length: number;
+  floorNumber: number;
+  description: string;
+  floorMap: number[][];
+}
+
+const FLOOR_DATA: FloorDTO[] = [
+  {
+    id: 'floor-id-1',
+    buildingId: 'building-id-1',
+    width: 10,
+    length: 10,
+    floorNumber: 1,
+    description: `Hydrogen is a chemical element with symbol H and atomic number 1. With a standard
+        atomic weight of 1.008, hydrogen is the lightest element on the periodic table.`,
+    floorMap: [[0,1,0,1,1],[0,0,0,0,0],[0,1,0,1,1],[0,1,0,1,1],[0,1,0,1,1]]
+  },
+  {
+    id: 'floor-id-2',
+    buildingId: 'building-id-2',
+    width: 10,
+    length: 10,
+    floorNumber: 2,
+    description: `Hydrogen is a chemical element with symbol H and atomic number 1. With a standard
+        atomic weight of 1.008, hydrogen is the lightest element on the periodic table.`,
+    floorMap: [[0,1,0,1,1],[0,0,0,0,0],[0,1,0,1,1],[0,1,0,1,1],[0,1,0,1,1]]
+  },
+  {
+    id: 'floor-id-3',
+    buildingId: 'building-id-2',
+    width: 10,
+    length: 10,
+    floorNumber: 3,
+    description: `Hydrogen is a chemical element with symbol H and atomic number 1. With a standard
+        atomic weight of 1.008, hydrogen is the lightest element on the periodic table.`,
+    floorMap: [[0,1,0,1,1],[0,0,0,0,0],[0,1,0,1,1],[0,1,0,1,1],[0,1,0,1,1]]
+  },
+];

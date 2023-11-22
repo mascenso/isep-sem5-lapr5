ce1c96cc315ae6d60467d76077741a874aae5a2d
Patrícia Teixeira <1210857@isep.ipp.pt>
2023-11-22 22:40:31 +0000

    [US1140] UI para ver lista de elevadores de um edificio (LPR5-57).

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -38,6 +38,7 @@ import { EditFloorsComponent } from "./home/campus/edit-floors/edit-floors.compo
 import { GlobalErrorHandler } from "./global-exception";
 import { CreateElevatorComponent } from './home/campus/create-elevator/create-elevator.component';
 import { ListBuildingsComponent } from './home/campus/list-buildings/list-buildings.component';
+import { ListElevatorsComponent } from './home/campus/list-elevators/list-elevators.component';
 
 @NgModule({
   declarations: [
@@ -57,7 +58,8 @@ import { ListBuildingsComponent } from './home/campus/list-buildings/list-buildi
     RobotListComponent,
     EditFloorsComponent,
     CreateElevatorComponent,
-    ListBuildingsComponent
+    ListBuildingsComponent,
+    ListElevatorsComponent
   ],
   imports: [
     BrowserModule,

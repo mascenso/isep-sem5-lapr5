409d3e154801acba0dfa3e1d39748d6080d926cc
miguelseixas <1100358@isep.ipp.pt>
2023-11-25 03:26:22 +0000

    [US1010] Permite criar Robots frontend LPR5-55

--- Frontend/src/dto/robotDTO.ts
+++ Frontend/src/dto/robotDTO.ts
@@ -6,3 +6,11 @@ export interface RobotDTO {
   description?: string;
   inhibited: boolean;
 }
+
+export interface RobotResponseDTO {
+  nickName: string;
+  robotType: string;
+  serialNumber: string;
+  description?: string;
+  inhibited: boolean;
+}

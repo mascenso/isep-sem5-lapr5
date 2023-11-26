1d91eaa841ade00a235877385385631b54707df6
mariana <1210855@isep.ipp.pt>
2023-11-26 19:09:05 +0000

    Adiciona teste - Create bridge

---
+++ Frontend/cypress/e2e/createBridge.spec.cy.ts
@@ -0,0 +1,30 @@
+describe('Teste de criar uma passagem', () => {
+
+  beforeEach(() => {
+    cy.visit('http://localhost:4200/login');
+    cy.get('#role').select('Administrador');
+    cy.get('#signUp').click();
+    cy.get('[id^="Campus"]').click();
+    cy.get('[routerlink="create-bridge"]').click();
+  });
+
+
+
+
+  it('Deve ser possivel criar edificio e receber mensagem de sucesso', () => {
+
+    //rota a interceptar para receber o codigo de sucesso
+    cy.intercept('POST', 'http://localhost:4000/api/bridges').as('rotaBridge');
+
+    cy.get('#mat-select-value-1').click();  // Click on the dropdown to open it
+    cy.get('mat-option').contains('A9426').click();  // Assuming 'A9426' is an option in the dropdown
+
+  })
+
+
+
+
+
+
+
+})

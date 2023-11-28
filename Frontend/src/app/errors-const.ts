dc6aef4632483c7b59701d73606eed309430d440
Patrícia Teixeira <1210857@isep.ipp.pt>
2023-11-28 01:07:45 +0000

    [US1140] Testes em cypress e dependencias adicionadas, WIP. (LPR5-51).

--- Frontend/src/app/errors-const.ts
+++ Frontend/src/app/errors-const.ts
@@ -1,5 +1,5 @@
 export const ErrorsConstant = {
     NetworkIssue: 'Check your internet connection and try again',
     ClientSide: 'Woops !! Your request could not be processed. Please try again later',
-    ServerSide: 'Sorrry !! Server encountered a temporary error. Pleae try again after sometime'
+    ServerSide: 'Sorrry !! Server encountered a temporary error. Please try again after sometime'
 }

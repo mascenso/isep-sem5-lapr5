f5aa0500ec85ff2837061548698eedebe7f30f1d
Patrícia Teixeira <1210857@isep.ipp.pt>
2023-12-21 23:06:49 +0000

    [US1430] Calcular tempo entre tarefas de forma dinamica, assim como o numero de tarefas (LPR5-117).

--- Planeamento/BC_RobDroneGo.pl
+++ Planeamento/BC_RobDroneGo.pl
@@ -125,20 +125,26 @@ pos_init(a2,[2,4]).
 pos_init(b2,[2,3]).
 pos_init(c3,[3,4]).
 
-% tarefas(NTarefas).
-tarefas(5).
 
-% tarefa(Id,TempoProcessamento,TempConc).
-tarefa(t1,2,5).
-tarefa(t2,4,7).
-tarefa(t3,1,11).
-tarefa(t4,3,9).
-tarefa(t5,3,8).
+% tarefa(Id,Origem,Destino).
+tarefa(t1,a104,a101).
+tarefa(t2,a202,a202).
+tarefa(t3,a202,a204).
+tarefa(t4,b201,b203).
+tarefa(t5,c305,c309).
 
-%tarefa_local(tarefa,piso,celula)
-tarefa_local(t1,a1,[2,2]).
-tarefa_local(t2,a2,[2,4]).
-tarefa_local(t3,a2,[3,3]).
-tarefa_local(t4,b2,[2,2]).
-tarefa_local(t5,c3,[4,4]).
+
+%localizacao(sala/gabinete/etc..,Piso, Celula)
+localizacao(a101,a1,[2,2]).
+localizacao(a104,a1,[2,3]).
+
+localizacao(a202,a2,[2,4]).
+localizacao(a204,a2,[3,3]).
+
+localizacao(b201,b2,[2,2]).
+localizacao(b203,b2,[2,3]).
+
+localizacao(c305,c3,[2,2]).
+localizacao(c309,c3,[4,4]).
+
 

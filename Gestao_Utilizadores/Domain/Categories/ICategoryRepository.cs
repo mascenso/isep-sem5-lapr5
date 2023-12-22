9ad8aad9b92ccf106b0465eae9d4d3d3ed22d90c
Luís Matos <1211804@isep.ipp.pt>
2023-12-22 22:53:07 +0000

    [US20] Limpeza (LPR5-100).

--- Gestao_Utilizadores/Domain/Categories/ICategoryRepository.cs
+++
@@ -1,9 +0,0 @@
-
-using UserManagement.Domain.Shared;
-
-namespace UserManagement.Domain.Categories
-{
-    public interface ICategoryRepository: IRepository<Category, CategoryId>
-    {
-    }
-}
--- Gestao_Utilizadores/Infrastructure/UserManagementDbContext.cs
+++ Gestao_Utilizadores/Infrastructure/UserManagementDbContext.cs
@@ -1,9 +1,4 @@
 using Microsoft.EntityFrameworkCore;
-using UserManagement.Domain.Categories;
-using UserManagement.Domain.Products;
-using UserManagement.Domain.Families;
-using UserManagement.Infrastructure.Categories;
-using UserManagement.Infrastructure.Products;
 using UserManagement.Domain.Users;
 using UserManagement.Infrastructure.Users;
 
@@ -11,12 +6,6 @@ namespace UserManagement.Infrastructure
 {
     public class UserManagementDbContext : DbContext
     {
-        public DbSet<Category> Categories { get; set; }
-
-        public DbSet<Product> Products { get; set; }
-
-        public DbSet<Family> Families { get; set; }
-
         public DbSet<User> Users { get; set; }
 
         public UserManagementDbContext(DbContextOptions options) : base(options)
@@ -26,9 +15,6 @@ namespace UserManagement.Infrastructure
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.ApplyConfiguration(new CategoryEntityTypeConfiguration());
-            modelBuilder.ApplyConfiguration(new ProductEntityTypeConfiguration());
-            modelBuilder.ApplyConfiguration(new FamilyEntityTypeConfiguration());
             modelBuilder.ApplyConfiguration(new UserEntityTypeConfiguration());
         }
     }

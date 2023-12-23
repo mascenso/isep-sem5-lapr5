ccfcedd3e41ef1728aef623054a13dc5d798d615
Luís Matos <1211804@isep.ipp.pt>
2023-12-23 13:08:05 +0000

    Adicionados scripts para compilar e arrancar com o módulo de utilizadores.

---
+++ Gestao_Utilizadores/build_and_run.sh
@@ -0,0 +1,14 @@
+﻿#!/bin/bash
+
+# Build the project
+dotnet build
+
+# Check if the build was successful
+if [ $? -eq 0 ]; then
+    echo "Build successful. Launching the project..."
+
+    # Run the project
+    dotnet run
+else
+    echo "Build failed. Please fix the issues before launching the project."
+fi

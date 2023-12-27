d22c5c88b05e89ca74fe3569a5b654afde88ca86
Patrícia Teixeira <1210857@isep.ipp.pt>
2023-12-27 00:57:29 +0000

    [US480&US500] Permite ver a lista de tarefas pendentes no frontend  (MVVM) - WIP (LPR5-96) LPR5-98).

--- Frontend/src/app/home/tasks/pending-task-list/pending-task-list.component.ts
+++ Frontend/src/app/home/tasks/pending-task-list/pending-task-list.component.ts
@@ -1,67 +1,29 @@
+// pending-task-list.component.ts
 import { Component, OnInit } from "@angular/core";
-import { TasksService } from "src/app/services/tasks.service";
-import { TaskPickupViewModel } from "src/app/viewModel/taskPickUp";
 import { TaskViewModel } from "src/app/viewModel/taskView";
-import { TaskVigilanceViewModel } from "src/app/viewModel/taskVigilance";
-
+import { TasksService } from "src/app/services/tasks.service";
 
 @Component({
   selector: 'app-pending-task-list',
   templateUrl: './pending-task-list.component.html',
   styleUrls: ['./pending-task-list.component.css']
 })
-
 export class PendingTaskListComponent implements OnInit {
-
   pendingTaskList: TaskViewModel[] = [];
   displayedColumns: string[] = ['description', 'user name', 'user contact'];
 
-  constructor(private tasksService: TasksService) {}
+  constructor(private taskService: TasksService) {}
 
   ngOnInit(): void {
-    this.carregarLista();
-  }
+    this.taskService.getAllPendingTasks().subscribe(
+      (tasks) => {
+        console.log("tasks ", tasks);
 
-  carregarLista() {
-    this.getPickupTasks();
-    this.getVigilanceTasks();
-  }
-
-getPickupTasks() {
-    this.tasksService.getAllPickupDeliveryPendingTasks().subscribe(
-      (pickupTasks) => {
-        const pickupTaskList= pickupTasks.flat();
-
-        const pickupTaskViewModels = pickupTaskList.map((task) => new TaskPickupViewModel(task));
-        console.log("pickupTaskList ", pickupTaskViewModels);
-        this.updatePendingTaskList(pickupTaskViewModels);
+        this.pendingTaskList = tasks;
       },
-      (pickupError) => {
-        console.error('Erro ao buscar as tarefas de pick up pendentes:', pickupError);
+      (error) => {
+        console.error('Erro ao buscar as tarefas pendentes:', error);
       }
     );
-  }
-
-getVigilanceTasks() {
-    this.tasksService.getAllVigilancePendingTasks().subscribe(
-      (vigilanceTasks) => {
-        const vigilanceTaskList= vigilanceTasks.flat();
-
-        const vigilanceTaskViewModels = vigilanceTaskList.map((task) => new TaskVigilanceViewModel(task));
-
-        console.log("vigilanceTasks ", vigilanceTaskViewModels);
-
-        this.updatePendingTaskList(vigilanceTaskViewModels);
-      },
-      (vigilanceError) => {
-        console.error('Erro ao buscar as tarefas de vigilância pendentes:', vigilanceError);
-      }
-    );
-  }
-
-updatePendingTaskList(tasks: TaskViewModel[]) {
-    this.pendingTaskList = this.pendingTaskList.concat(tasks);
-    console.log("pendingTaskList ", this.pendingTaskList);
-
   }
 }

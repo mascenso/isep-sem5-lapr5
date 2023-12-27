d4e7192aaa6e28c985b0c2556d41823a68ba7786
Patrícia Teixeira <1210857@isep.ipp.pt>
2023-12-27 18:50:05 +0000

    [US480&US500] Permite ver a lista de tarefas pendentes no frontend  (MVC) - WORKS (LPR5-96) LPR5-98).

--- Frontend/src/app/services/tasks.service.ts
+++ Frontend/src/app/services/tasks.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import {environment} from "../../environments/environment";
-import {HttpClient} from "@angular/common/http";
-import {Observable, map} from "rxjs";
+import { environment } from "../../environments/environment";
+import { HttpClient } from "@angular/common/http";
+import { Observable, forkJoin, map, mergeMap, of } from "rxjs";
 import { TaskVigilanceRequestDTO } from "../../dto/taskVigilanceDTO";
 import { TaskPickupRequestDTO } from 'src/dto/taskPickupDTO';
 import { TaskViewModel } from '../viewModel/taskView';
@@ -23,45 +23,81 @@ export class TasksService {
     return this.http.post<TaskPickupRequestDTO[]>(`${this.API_URL}/api/tasks/pickupDelivery`, pickupTask);
   }
 
-  
+
   public getAllVigilancePendingTasks(): Observable<TaskVigilanceRequestDTO[]> {
     return this.http.get<TaskVigilanceRequestDTO[]>(`${this.API_URL}/api/tasks/pendingVigilance`);
   }
-
+  
   public getAllPickupDeliveryPendingTasks(): Observable<TaskPickupRequestDTO[]> {
     return this.http.get<TaskPickupRequestDTO[]>(`${this.API_URL}/api/tasks/pendingPickUp`);
   }
 
 
-
+ /* 
   public getAllPendingTasks(): Observable<TaskViewModel[]> {
-    return this.http.get<any[]>(`${this.API_URL}/api/tasks/pending`).pipe(
-      map((dataArray: any[][]) => this.mapDataToViewModel(dataArray))
+    const pickupTasks$ = this.getAllPickupDeliveryPendingTasks().pipe(
+      mergeMap(tasks => this.mapToViewModelPickUp(tasks)),
+    );
+  
+    const vigilanceTasks$ = this.getAllVigilancePendingTasks().pipe(
+      mergeMap(tasks => this.mapToViewModelVigilance(tasks)),
+    );
+  
+    // Combine the results using forkJoin to wait for both observables to complete
+    return forkJoin([pickupTasks$, vigilanceTasks$]).pipe(
+      // Merge the arrays into a single array of TaskViewModel
+      map(([pickupTasksResult, vigilanceTasksResult]) => [...pickupTasksResult, ...vigilanceTasksResult]),
     );
   }
 
-  private mapDataToViewModel(dataArray: any[][]): TaskViewModel[] {
+  private mapToViewModelPickUp(dataArray: TaskPickupRequestDTO[]): Observable<TaskViewModel[]> {
+    return of(dataArray.map(dataSet => this.mapToViewModel(dataSet)));
+  }
+  
+  private mapToViewModelVigilance(dataArray: TaskVigilanceRequestDTO[]): Observable<TaskViewModel[]> {
+    return of(dataArray.map(dataSet => this.mapToViewModel(dataSet)));
+  }
+
+
+  private mapToViewModelPickUp(dataArray: TaskPickupRequestDTO[]): TaskViewModel[] {
     const tasks: TaskViewModel[] = [];
-
     dataArray.forEach(dataSet => {
-      dataSet.forEach(data => {
-        tasks.push(this.mapToViewModel(data));
-      });
+        tasks.push(this.mapToViewModel(dataSet));
     });
-
     return tasks;
   }
 
-  private mapToViewModel(data: any): TaskViewModel {
-    console.log('data ', data);
-    const userName =data.user.name;
-    const userContact = data.user.contacto;
+  private mapToViewModelVigilance(dataArray: TaskVigilanceRequestDTO[]): TaskViewModel[] {
+    const tasks: TaskViewModel[] = [];
+    dataArray.forEach(dataSet => {
+        tasks.push(this.mapToViewModel(dataSet));
+    });
+    return tasks;
+  }
+  
+  private mapToViewModel(dataArray: any[]): TaskViewModel[] {
+    const result: TaskViewModel[] = [];
+  
+    dataArray.forEach(data => {
+      // Aqui, data representa um item do array dentro do array maior
+      const mappedData = this.mapSingleToViewModel(data);
+      result.push(mappedData);
+    });
+  
+    return result;
+  }
+  
+  private mapSingleToViewModel(data: any): TaskViewModel {
 
+    console.log("final ", data.user.name);
+    // Aqui, data representa um único item do array
     return {
-      id: data.id || '',
-      description: data.description || '',
-      user: userName,
-      contact: userContact,
+      id: data.id,
+      description: data.description,
+      user: data.user.name,
+      contact: data.user.contact,
+      // Mapear outras propriedades conforme necessário
     };
   }
+*/
 }

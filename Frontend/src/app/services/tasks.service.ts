812ec29a1391d9f764d3b845b0073f9294a8dcb6
Patrícia Teixeira <1210857@isep.ipp.pt>
2023-12-27 00:57:29 +0000

    [US480&US500] Permite ver a lista de tarefas pendentes no frontend  (MVVM) - WIP (LPR5-96) LPR5-98).

--- Frontend/src/app/services/tasks.service.ts
+++ Frontend/src/app/services/tasks.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import {environment} from "../../environments/environment";
 import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
+import {Observable, map} from "rxjs";
 import { TaskVigilanceRequestDTO } from "../../dto/taskVigilanceDTO";
 import { TaskPickupRequestDTO } from 'src/dto/taskPickupDTO';
+import { TaskViewModel } from '../viewModel/taskView';
 
 @Injectable({
   providedIn: 'root'
@@ -22,24 +23,45 @@ export class TasksService {
     return this.http.post<TaskPickupRequestDTO[]>(`${this.API_URL}/api/tasks/pickupDelivery`, pickupTask);
   }
 
+  /*
   public getAllVigilancePendingTasks(): Observable<TaskVigilanceRequestDTO[]> {
-    console.log("VIGILANCE SERVIÇO ");
-
     return this.http.get<TaskVigilanceRequestDTO[]>(`${this.API_URL}/api/tasks/pendingVigilance`);
   }
 
   public getAllPickupDeliveryPendingTasks(): Observable<TaskPickupRequestDTO[]> {
-    console.log("PICK UP SERVIÇO ");
-
     return this.http.get<TaskPickupRequestDTO[]>(`${this.API_URL}/api/tasks/pendingPickUp`);
   }
-/*
-  public getAllPendingTasks(): Observable<any[]> {
-    let pendingPickUp = this.getAllPickupDeliveryPendingTasks;
-    let pendingVigilance = this.getAllVigilancePendingTasks;
-
-    const allPendingTasks: any[] = [ pendingPickUp,pendingVigilance];
-    return allPendingTasks;
-  }
 */
+
+
+  public getAllPendingTasks(): Observable<TaskViewModel[]> {
+    return this.http.get<any[]>(`${this.API_URL}/api/tasks/pending`).pipe(
+      map((dataArray: any[][]) => this.mapDataToViewModel(dataArray))
+    );
+  }
+
+  private mapDataToViewModel(dataArray: any[][]): TaskViewModel[] {
+    const tasks: TaskViewModel[] = [];
+
+    dataArray.forEach(dataSet => {
+      dataSet.forEach(data => {
+        tasks.push(this.mapToViewModel(data));
+      });
+    });
+
+    return tasks;
+  }
+
+  private mapToViewModel(data: any): TaskViewModel {
+    console.log('data ', data);
+    const userName =data.user.name;
+    const userContact = data.user.contacto;
+
+    return {
+      id: data.id || '',
+      description: data.description || '',
+      user: userName,
+      contact: userContact,
+    };
+  }
 }

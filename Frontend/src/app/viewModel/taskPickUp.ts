0a8fa07079d16dac9d581699e3c26d97248e13e6
Patrícia Teixeira <1210857@isep.ipp.pt>
2023-12-27 19:45:21 +0000

    [US480] Permite ver a lista de tarefas pendentes no frontend  (MVVM) (LPR5-96).

--- Frontend/src/app/viewModel/taskPickUp.ts
+++ Frontend/src/app/viewModel/taskPickUp.ts
@@ -1,41 +1,26 @@
 import { TaskViewModel } from "./taskView";
 
-export class TaskPickupViewModel extends TaskViewModel {
-    pickupLocalization: {
-      buildingId: string;
-      floor: object;
-      room: number[];
-    };
-    deliveryLocalization: {
-      buildingId: string;
-      floor: object;
-      room: number[];
-    };
+export interface TaskPickupViewModel extends TaskViewModel {
+  pickupLocalization: {
+    buildingId: string;
+    floor: object;
+    room: number[];
+  };
+  deliveryLocalization: {
+    buildingId: string;
+    floor: object;
+    room: number[];
+  };
+  contactNumber: number;
+  deliveryContact: {
+    name: string;
     contactNumber: number;
-    deliveryContact: {
-      name: string;
-      contactNumber: number;
-    };
-    pickupContact: {
-      name: string;
-      contactNumber: number;
-    };
-    approved: boolean;
-    pending: boolean;
-    planned: boolean;
-
-
-
-    constructor(data: any) {
-      super(data); 
-      this.pickupLocalization = data.pickupLocalization;
-      this.deliveryLocalization = data.deliveryLocalization;
-      this.contactNumber = data.contactNumber;
-      this.deliveryContact = data.deliveryContact;
-      this.pickupContact = data.pickupContact;
-      this.approved = data.approved;
-      this.pending = data.pending;
-      this.planned=data.planned
-
-    }
-  }
+  };
+  pickupContact: {
+    name: string;
+    contactNumber: number;
+  };
+  approved: boolean;
+  pending: boolean;
+  planned: boolean;
+}

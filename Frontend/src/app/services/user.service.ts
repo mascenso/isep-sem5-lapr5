17b1c7bd3fbe23c61c8bc50747d43ddafb6ec24d
Luís Matos <1211804@isep.ipp.pt>
2023-12-28 16:56:46 +0000

    [US100] Limpeza (LRP5-107).

--- Frontend/src/app/services/user.service.ts
+++ Frontend/src/app/services/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { UserResponseDTO, CreateUserRequestDTO } from '../../dto/userDTO';
 import {PatchUserDataRequestDTO} from "../../dto/patchUserDataRequestDTO";
@@ -15,30 +15,12 @@ export class UserService {
   constructor(private http: HttpClient) { }
 
   public registerUser(user: CreateUserRequestDTO, showSpinner?: boolean): Observable<UserResponseDTO> {
-    const headers = new HttpHeaders({
-        'Content-Type': 'application/json',
-    });
-
-    const options = {
-        headers: headers,
-        reportProgress: showSpinner
-    };
-
-    console.log(user);
-
-    //return this.http.post<UserResponseDTO>(`${this.API_URL}/api/users/`, user, options);
     return this.http.post<UserResponseDTO>(`${this.API_URL}/api/users/register-system-user`, user);
   }
 
   public GetInactiveUsers(): Observable<UserResponseDTO[]> {
     return this.http.get<UserResponseDTO[]>(`${this.API_URL}/api/users/inactive`);
   }
-
-  /*
-  public getAllUsers(): Observable<UserResponseDTO[]> {
-    return this.http.get<UserResponseDTO[]>(`${this.API_URL}/api/users/`);
-  }
-  */
 
   public updateUser(user: PatchUserDataRequestDTO): Observable<UserResponseDTO> {
     return this.http.patch<UserResponseDTO>(`${this.API_URL}/api/users/patch-user`, user);

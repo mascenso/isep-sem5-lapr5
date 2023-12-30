3f7fec274ae4ab182d6a6402bf12da8eedd4e902
Patrícia Teixeira <1210857@isep.ipp.pt>
2023-12-30 17:15:45 +0000

    [US500] Adiciona novo atributo das vigilance tasks à view (LPR5-98).

--- Frontend/src/app/model/taskVigilance.ts
+++ Frontend/src/app/model/taskVigilance.ts
@@ -3,6 +3,8 @@ import { Task } from "./task";
 export interface TaskVigilance extends Task {
     buildingId: string;
     floors: object[];
+    startPosition: number[];
+    endPosition: number[];
     contactNumber: number;
     approved: boolean;
     pending: boolean;

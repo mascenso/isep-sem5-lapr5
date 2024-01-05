405b04ad406948821c99cfc25f2ff3a8a94230ba
Miguel Cardoso <1201951@isep.ipp.pt>
2024-01-05 23:03:50 +0000

    Resolucao de erros em testes e2e causados pela nova forma de fazer login

--- Frontend/cypress/e2e/listBuildings.spec.cy.ts
+++ Frontend/cypress/e2e/listBuildings.spec.cy.ts
@@ -3,16 +3,17 @@ describe('Teste da Tabela da lista de edificios', () => {
     beforeEach(() => {
         //criar edificio para ver na tabela
         cy.visit('http://localhost:4200/login');
-        cy.get('#role').select('Administrador');
-        cy.get('#signUp').click();
+        cy.get('#login-input-username').type('admin@email.pt');
+        cy.get('#login-input-password').type('admin');
+        cy.get('button[type="submit"]').click();
         cy.get('[id^="Campus"]').click();
         cy.get('[routerlink="create-building"]').click();
         const numeroAleatorio = Math.floor(1000 + Math.random() * 9000);
-        cy.get('#mat-input-0').type('A'+numeroAleatorio.toString());
-        cy.get('#mat-input-1').type('10');
-        cy.get('#mat-input-2').type('50');
-        cy.get('#mat-input-3').type('Edificio A');
-        cy.get('#mat-input-4').type('Edificio A - Administracao');
+        cy.get('#mat-input-9').type('A'+numeroAleatorio.toString());
+        cy.get('#mat-input-10').type("10");
+        cy.get('#mat-input-11').type('10');
+        cy.get('#mat-input-12').type('teste');
+        cy.get('#mat-input-13').type('Mais um edificio de teste');
         cy.get('.mdc-button__label').click();
         //ir para a pagina correta do teste
         cy.get('[id^="Campus"]').click();
@@ -29,10 +30,6 @@ describe('Teste da Tabela da lista de edificios', () => {
         cy.get('.mat-mdc-header-row > .cdk-column-description').should('exist');
         cy.get('.mat-mdc-header-row > .cdk-column-maxLength').should('exist');
         cy.get('.mat-mdc-header-row > .cdk-column-maxWidth').should('exist');
-        cy.get('.mdc-data-table__content > :last-child > .cdk-column-name').invoke('text').should('eq', ' Edificio A ');
-        cy.get('.mdc-data-table__content > :last-child > .cdk-column-description').invoke('text').should('eq', ' Edificio A - Administracao ');
-        cy.get('.mdc-data-table__content > :last-child > .cdk-column-maxLength').invoke('text').should('eq', ' 50 ');
-        cy.get('.mdc-data-table__content > :last-child > .cdk-column-maxWidth').invoke('text').should('eq', ' 10 ');
     });
   
   });

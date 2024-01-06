e84b1ee9a5eee8f8b3591dd955dd1780e77405b7
mariana <1210855@isep.ipp.pt>
2024-01-06 00:01:59 +0000

    Melhorias gerais - Mód Tarefas: VO LocationRoom

--- Gestao_Tarefas/src/dto/ITaskPickupDeliveryDTO.ts
+++ Gestao_Tarefas/src/dto/ITaskPickupDeliveryDTO.ts
@@ -1,18 +1,11 @@
 import { User } from "../domain/task-agg/user";
+import { LocationRoom } from "../domain/task-agg/locationRoom";
 
 export default interface ITaskPickupDeliveryDTO {
   id: string;
   description: string;
-  pickupLocalization: {
-    buildingId:String;
-    floor:object;
-    room: number[];
-  };
-  deliveryLocalization:{
-    buildingId:String;
-    floor:object;
-    room: number[];
-  };
+  pickupLocalization: LocationRoom;
+  deliveryLocalization: LocationRoom;
   contactNumber:number;
   user:User;
   deliveryContact:User;

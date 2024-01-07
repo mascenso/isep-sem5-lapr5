5cde7110ce2515cbcec68a1b2da1f1ec9a080c79
mariana <1210855@isep.ipp.pt>
2024-01-07 17:41:05 +0000

    Corrige teste unit-taskPickUp.test.ts

--- Gestao_Tarefas/tests/unit-taskPickUp.test.ts
+++ Gestao_Tarefas/tests/unit-taskPickUp.test.ts
@@ -2,31 +2,23 @@ import { expect } from 'chai';
 import { UniqueEntityID } from '../src/core/domain/UniqueEntityID';
 import { TaskPickupDelivery } from '../src/domain/task-agg/TaskPickupDelivery';
 import { LocationRoom } from '../src/domain/task-agg/locationRoom';
+import { TaskStatusVO } from "../src/domain/task-agg/taskStatusVO";
+import { User } from "../src/domain/task-agg/user";
 
 
-describe('Task PickUp - Unit Test', () => { 
-   /*
+describe('Task PickUp - Unit Test', () => {
+
   it('should create a valid TaskPickUp instance', () => {
-  
+
     const taskProps = {
       description: 'Pickup Task 1',
-      pickupLocalization: { buildingId: '12', floor: {}, room: [1, 2]},
-      deliveryLocalization: { buildingId: '1212', floor: {}, room: [4, 2]},
+      pickupLocalization: LocationRoom.create('12', {"floorId":"id-muito-grande","floorNumber":3, "description":"a1"}, [1, 2]).getValue(),
+      deliveryLocalization: LocationRoom.create('1212', {"floorId":"id-muito-grande","floorNumber":3, "description":"a1"}, [4, 2]).getValue(),
       contactNumber: 123456789,
-      user: {
-        userName:'Zé', contactNumber: 987654321
-      },
-      deliveryContact: {
-        name: 'Zé',
-        contactNumber: 987654321
-      },
-      pickupContact: {
-        name: 'Rui',
-        contactNumber: 98172322
-      },
-      approved: true,
-      pending: false,
-      planned: false
+      user: User.create('Zé', 987654321, '1210888@isep.ipp.pt').getValue(),
+      deliveryContact: User.create('Zé', 987654321, '1210888@isep.ipp.pt').getValue(),
+      pickupContact: User.create('Rui', 981722222, '1111111@isep.ipp.pt').getValue(),
+      taskStatus: TaskStatusVO.create( true, false, false ).getValue()
     };
 
     // Cria a tarefa de pickup delivery
@@ -46,9 +38,9 @@ describe('Task PickUp - Unit Test', () => {
     expect(taskPickupDelivery.user).to.deep.equal(taskProps.user);
     expect(taskPickupDelivery.deliveryContact).to.deep.equal(taskProps.deliveryContact);
     expect(taskPickupDelivery.pickupContact).to.deep.equal(taskProps.pickupContact);
-    expect(taskPickupDelivery.approved).to.equal(taskProps.approved);
-    expect(taskPickupDelivery.pending).to.equal(taskProps.pending);
-    expect(taskPickupDelivery.planned).to.equal(taskProps.planned);
+    expect(taskPickupDelivery.taskStatus.approved).to.equal(taskProps.taskStatus.approved);
+    expect(taskPickupDelivery.taskStatus.pending).to.equal(taskProps.taskStatus.pending);
+    expect(taskPickupDelivery.taskStatus.planned).to.equal(taskProps.taskStatus.planned);
   });
-  */
+
 });
